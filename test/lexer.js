e707c22c36c5113fe89c42030b1c8d0f3b9b96fc
Alberto La Rocca <a71104@gmail.com>
2014-10-12 10:43:18 +0200

    Lexer test framework

--- test/lexer.js
+++ test/lexer.js
@@ -1,56 +1,54 @@
 var Lambda = require('../bin/lambda.js');
 
-function createLexer(text) {
-	return new Lambda.Lexer(text);
-}
-
-module.exports.testEmpty = function (test) {
-	var lexer = createLexer('');
-	test.ok(lexer.end());
-	test.done();
-};
-
-module.exports.testWhiteSpace = function (test) {
-	var lexer = createLexer('');
-	test.ok(lexer.end());
-	test.done();
-};
-
-
-function testKeyword(keyword) {
+function testTokens(string, tokens) {
 	return function (test) {
-		var lexer = createLexer(keyword);
-		test.ok(lexer.getCurrent() === 'keyword:' + keyword);
-		test.ok(lexer.getLabel() === keyword);
-		test.ok(lexer.next() === 'end');
+		var lexer = new Lambda.Lexer(string);
+		tokens.forEach(function (token) {
+			test.ok(!lexer.end());
+			if (typeof token === 'string') {
+				test.ok(lexer.getCurrent() === token);
+			} else {
+				test.ok(lexer.getCurrent() === token.type);
+				test.ok(lexer.getLabel() === token.label);
+			}
+			lexer.next();
+		});
+		test.ok(lexer.end());
+		test.ok(lexer.getCurrent() === 'end');
 		test.done();
 	};
 }
 
-module.exports.testNullKeyword = testKeyword('null');
-module.exports.testUndefinedKeyword = testKeyword('undefined');
-module.exports.testTrueKeyword = testKeyword('true');
-module.exports.testFalseKeyword = testKeyword('false');
-module.exports.testNotKeyword = testKeyword('not');
-module.exports.testAndKeyword = testKeyword('and');
-module.exports.testOrKeyword = testKeyword('or');
-module.exports.testXorKeyword = testKeyword('xor');
-module.exports.testFixKeyword = testKeyword('fix');
-module.exports.testThisKeyword = testKeyword('this');
-module.exports.testNewKeyword = testKeyword('new');
-module.exports.testBoolKeyword = testKeyword('bool');
-module.exports.testUnknownKeyword = testKeyword('unknown');
-module.exports.testIntKeyword = testKeyword('int');
-module.exports.testFloatKeyword = testKeyword('float');
-module.exports.testStringKeyword = testKeyword('string');
-module.exports.testRegexKeyword = testKeyword('regex');
-module.exports.testLetKeyword = testKeyword('let');
-module.exports.testInKeyword = testKeyword('in');
-module.exports.testIfKeyword = testKeyword('if');
-module.exports.testThenKeyword = testKeyword('then');
-module.exports.testElseKeyword = testKeyword('else');
-module.exports.testThrowKeyword = testKeyword('throw');
-module.exports.testTryKeyword = testKeyword('try');
-module.exports.testCatchKeyword = testKeyword('catch');
-module.exports.testFinallyKeyword = testKeyword('finally');
-module.exports.testErrorKeyword = testKeyword('error');
+module.exports.testEmpty = testTokens('', []);
+module.exports.testWhiteSpace1 = testTokens(' ', []);
+module.exports.testWhiteSpace2 = testTokens('\t', []);
+module.exports.testWhiteSpace3 = testTokens('\n', []);
+module.exports.testWhiteSpace4 = testTokens(' \t\n', []);
+
+module.exports.testNullKeyword = testTokens('null', ['keyword:null']);
+module.exports.testUndefinedKeyword = testTokens('undefined', ['keyword:undefined']);
+module.exports.testTrueKeyword = testTokens('true', ['keyword:true']);
+module.exports.testFalseKeyword = testTokens('false', ['keyword:false']);
+module.exports.testNotKeyword = testTokens('not', ['keyword:not']);
+module.exports.testAndKeyword = testTokens('and', ['keyword:and']);
+module.exports.testOrKeyword = testTokens('or', ['keyword:or']);
+module.exports.testXorKeyword = testTokens('xor', ['keyword:xor']);
+module.exports.testFixKeyword = testTokens('fix', ['keyword:fix']);
+module.exports.testThisKeyword = testTokens('this', ['keyword:this']);
+module.exports.testNewKeyword = testTokens('new', ['keyword:new']);
+module.exports.testBoolKeyword = testTokens('bool', ['keyword:bool']);
+module.exports.testUnknownKeyword = testTokens('unknown', ['keyword:unknown']);
+module.exports.testIntKeyword = testTokens('int', ['keyword:int']);
+module.exports.testFloatKeyword = testTokens('float', ['keyword:float']);
+module.exports.testStringKeyword = testTokens('string', ['keyword:string']);
+module.exports.testRegexKeyword = testTokens('regex', ['keyword:regex']);
+module.exports.testLetKeyword = testTokens('let', ['keyword:let']);
+module.exports.testInKeyword = testTokens('in', ['keyword:in']);
+module.exports.testIfKeyword = testTokens('if', ['keyword:if']);
+module.exports.testThenKeyword = testTokens('then', ['keyword:then']);
+module.exports.testElseKeyword = testTokens('else', ['keyword:else']);
+module.exports.testThrowKeyword = testTokens('throw', ['keyword:throw']);
+module.exports.testTryKeyword = testTokens('try', ['keyword:try']);
+module.exports.testCatchKeyword = testTokens('catch', ['keyword:catch']);
+module.exports.testFinallyKeyword = testTokens('finally', ['keyword:finally']);
+module.exports.testErrorKeyword = testTokens('error', ['keyword:error']);
