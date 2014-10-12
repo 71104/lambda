998627d845e7ea3a103af21eb3308694d333c76a
Alberto La Rocca <a71104@gmail.com>
2014-10-12 10:28:44 +0200

    Fixing operators (`not` instead of `!` and correct implementation of `xor`)

--- src/DefaultContext.js
+++ src/DefaultContext.js
@@ -11,7 +11,7 @@ var DefaultContext = exports.DefaultContext = function () {
 		return that.push(symbol, (new LambdaNode('0', null, new LambdaNode('1', null, new NativeNode(operator, null, ['0', '1'])))).evaluate(that));
 	}
 
-	pushUnaryOperator('!', function (x) { return !x; });
+	pushUnaryOperator('not', function (x) { return !x; });
 	pushUnaryOperator('~', function (x) { return ~x; });
 
 	pushBinaryOperator('+', function (x, y) { return x + y; });
@@ -29,6 +29,12 @@ var DefaultContext = exports.DefaultContext = function () {
 	pushBinaryOperator('>=', function (x, y) { return x >= y; });
 	pushBinaryOperator('<=', function (x, y) { return x <= y; });
 	pushBinaryOperator('**', function (x, y) { return Math.pow(x, y); });
+	pushBinaryOperator('and', function (x, y) { return !!(x && y); });
+	pushBinaryOperator('or', function (x, y) { return !!(x || y); });
+
+	/*jshint ignore: start */
+	pushBinaryOperator('xor', function (x, y) { return !!x !== !!y; });
+	/*jshint ignore: end */
 };
 
 DefaultContext.prototype = Object.create(Context.prototype);
