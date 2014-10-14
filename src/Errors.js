6bbbfc5a687c47553316a6e26b03fd79d6a3ef92
Alberto La Rocca <a71104@gmail.com>
2014-10-14 22:14:35 +0200

    LambdaUserError must be a regular function because it must be recognizable through instanceof

--- src/Errors.js
+++ src/Errors.js
@@ -1,43 +1,23 @@
-var LambdaError = exports.LambdaError = function (description) {
-	this.error = new Error(description);
-};
-
-LambdaError.prototype.toString = function () {
-	return this.error.toString();
-};
-
-
 var LambdaInternalError = exports.InternalError = function () {
-	LambdaError.call(this, 'internal error');
+	return Error.call(this, 'internal error');
 };
-
-LambdaInternalError.prototype = Object.create(LambdaError.prototype);
 
 
 var LambdaSyntaxError = exports.SyntaxError = function () {
-	LambdaError.call(this, 'syntax error');
+	return Error.call(this, 'syntax error');
 };
-
-LambdaSyntaxError.prototype = Object.create(LambdaError.prototype);
 
 
 var LambdaTypeError = exports.TypeError = function () {
-	LambdaError.call(this, 'type error');
+	return Error.call(this, 'type error');
 };
-
-LambdaTypeError.prototype = Object.create(LambdaError.prototype);
 
 
 var LambdaRuntimeError = exports.RuntimeError = function () {
-	LambdaError.call(this, 'runtime error');
+	return Error.call(this, 'runtime error');
 };
-
-LambdaRuntimeError.prototype = Object.create(LambdaError.prototype);
 
 
 var LambdaUserError = exports.UserError = function (value) {
-	LambdaError.call(this, 'user error: ' + value);
 	this.value = value;
 };
-
-LambdaUserError.prototype = Object.create(LambdaError.prototype);
