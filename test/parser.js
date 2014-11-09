537829288223fd08827b267ca27173a11d61b2b9
Alberto La Rocca <a71104@gmail.com>
2014-11-09 10:46:56 +0100

    Testing

--- test/parser.js
+++ test/parser.js
@@ -135,3 +135,147 @@ module.exports.testVariableFieldAccess = function (test) {
 	test.ok(ast.name === 'field');
 	test.done();
 };
+
+module.exports.testSubscript1 = function (test) {
+	var ast = parse('x[0]');
+	test.ok(ast.is(Lambda.SubscriptNode));
+	test.ok(ast.expression.is(Lambda.VariableNode));
+	test.ok(ast.expression.name === 'x');
+	test.ok(ast.index.is(Lambda.LiteralNode));
+	test.done();
+};
+
+module.exports.testSubscript2 = function (test) {
+	var ast = parse('0[x]');
+	test.ok(ast.is(Lambda.SubscriptNode));
+	test.ok(ast.expression.is(Lambda.LiteralNode));
+	test.ok(ast.index.is(Lambda.VariableNode));
+	test.ok(ast.index.name === 'x');
+	test.done();
+};
+
+module.exports.testPolymorphicLambda1 = function (test) {
+	var ast = parse('x -> y');
+	test.ok(ast.is(Lambda.LambdaNode));
+	test.ok(ast.name === 'x');
+	test.ok(ast.type === null);
+	test.ok(ast.body.is(Lambda.VariableNode));
+	test.ok(ast.body.name === 'y');
+	test.done();
+};
+
+module.exports.testPolymorphicLambda2 = function (test) {
+	var ast = parse('y -> x');
+	test.ok(ast.is(Lambda.LambdaNode));
+	test.ok(ast.name === 'y');
+	test.ok(ast.type === null);
+	test.ok(ast.body.is(Lambda.VariableNode));
+	test.ok(ast.body.name === 'x');
+	test.done();
+};
+
+module.exports.testPolymorphicLambda3 = function (test) {
+	var ast = parse('y -> x');
+	test.ok(ast.is(Lambda.LambdaNode));
+	test.ok(ast.name === 'y');
+	test.ok(ast.type === null);
+	test.ok(ast.body.is(Lambda.VariableNode));
+	test.ok(ast.body.name === 'x');
+	test.done();
+};
+
+module.exports.testMultiplePolymorphicLambda1 = function (test) {
+	var ast = parse('x, y -> 0');
+	test.ok(ast.is(Lambda.LambdaNode));
+	test.ok(ast.name === 'x');
+	test.ok(ast.type === null);
+	test.ok(ast.body.is(Lambda.LambdaNode));
+	test.ok(ast.body.name === 'y');
+	test.ok(ast.body.type === null);
+	test.ok(ast.body.body.is(Lambda.LiteralNode));
+	test.ok(ast.body.body.value.is(Lambda.IntegerValue));
+	test.ok(ast.body.body.value.value === 0);
+	test.ok(ast.body.body.type.is(Lambda.IntegerType));
+	test.done();
+};
+
+module.exports.testMultiplePolymorphicLambda2 = function (test) {
+	var ast = parse('y, x -> false');
+	test.ok(ast.is(Lambda.LambdaNode));
+	test.ok(ast.name === 'y');
+	test.ok(ast.type === null);
+	test.ok(ast.body.is(Lambda.LambdaNode));
+	test.ok(ast.body.name === 'x');
+	test.ok(ast.body.type === null);
+	test.ok(ast.body.body.is(Lambda.LiteralNode));
+	test.ok(ast.body.body.value.is(Lambda.BooleanValue));
+	test.ok(ast.body.body.value.value === false);
+	test.ok(ast.body.body.type.is(Lambda.BooleanType));
+	test.done();
+};
+
+module.exports.testTypedLambda1 = function (test) {
+	var ast = parse('x: int -> y');
+	test.ok(ast.is(Lambda.LambdaNode));
+	test.ok(ast.name === 'x');
+	test.ok(ast.type.is(Lambda.IntegerType));
+	test.ok(ast.body.is(Lambda.VariableNode));
+	test.ok(ast.body.name === 'y');
+	test.done();
+};
+
+module.exports.testTypedLambda2 = function (test) {
+	var ast = parse('y: float -> x');
+	test.ok(ast.is(Lambda.LambdaNode));
+	test.ok(ast.name === 'y');
+	test.ok(ast.type.is(Lambda.FloatType));
+	test.ok(ast.body.is(Lambda.VariableNode));
+	test.ok(ast.body.name === 'x');
+	test.done();
+};
+
+module.exports.testMultipleTypedLambda1 = function (test) {
+	var ast = parse('x: string, y: complex -> z');
+	test.ok(ast.is(Lambda.LambdaNode));
+	test.ok(ast.name === 'x');
+	test.ok(ast.type.is(Lambda.StringType));
+	test.ok(ast.body.is(Lambda.LambdaNode));
+	test.ok(ast.body.name === 'y');
+	test.ok(ast.body.type.is(Lambda.ComplexType));
+	test.ok(ast.body.body.is(Lambda.VariableNode));
+	test.ok(ast.body.body.name === 'z');
+	test.done();
+};
+
+module.exports.testMultipleTypedLambda2 = function (test) {
+	var ast = parse('z: complex, x: string -> y');
+	test.ok(ast.is(Lambda.LambdaNode));
+	test.ok(ast.name === 'z');
+	test.ok(ast.type.is(Lambda.ComplexType));
+	test.ok(ast.body.is(Lambda.LambdaNode));
+	test.ok(ast.body.name === 'x');
+	test.ok(ast.body.type.is(Lambda.StringType));
+	test.ok(ast.body.body.is(Lambda.VariableNode));
+	test.ok(ast.body.body.name === 'y');
+	test.done();
+};
+
+module.exports.testApplication1 = function (test) {
+	var ast = parse('x y');
+	test.ok(ast.is(Lambda.ApplicationNode));
+	test.ok(ast.left.is(Lambda.VariableNode));
+	test.ok(ast.left.name === 'x');
+	test.ok(ast.right.is(Lambda.VariableNode));
+	test.ok(ast.right.name === 'y');
+	test.done();
+};
+
+module.exports.testApplication2 = function (test) {
+	var ast = parse('y x');
+	test.ok(ast.is(Lambda.ApplicationNode));
+	test.ok(ast.left.is(Lambda.VariableNode));
+	test.ok(ast.left.name === 'y');
+	test.ok(ast.right.is(Lambda.VariableNode));
+	test.ok(ast.right.name === 'x');
+	test.done();
+};
