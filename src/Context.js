453f040642679a3f23238fcf1686109220c0a751
Alberto La Rocca <a71104@gmail.com>
2014-12-28 17:30:29 +0100

    Fixing #34

--- src/Context.js
+++ src/Context.js
@@ -1,12 +1,23 @@
 var Context = exports.Context = function (hash) {
-	this.hash = hash || {};
+	this.hash = {};
+	if (hash) {
+		for (var name in hash) {
+			if (hash.hasOwnProperty(name)) {
+				this.hash[Context.PREFIX + name] = hash[name];
+			}
+		}
+	}
 };
 
+Context.PREFIX = 'LAMBDA ';
+
 Context.prototype.has = function (name) {
+	name = Context.PREFIX + name;
 	return this.hash.hasOwnProperty(name);
 };
 
 Context.prototype.top = function (name) {
+	name = Context.PREFIX + name;
 	if (this.hash.hasOwnProperty(name)) {
 		return this.hash[name];
 	} else {
@@ -17,7 +28,7 @@ Context.prototype.top = function (name) {
 Context.prototype.forEach = function (callback, context) {
 	for (var name in this.hash) {
 		if (this.hash.hasOwnProperty(name)) {
-			callback.call(context || null, name, this.hash[name]);
+			callback.call(context || null, name.substr(Context.PREFIX.length), this.hash[name]);
 		}
 	}
 };
@@ -29,6 +40,8 @@ Context.prototype.add = function (newName, value) {
 			hash[name] = this.hash[name];
 		}
 	}
-	hash[newName] = value;
-	return new Context(hash);
+	hash[Context.PREFIX + newName] = value;
+	var context = new Context(hash);
+	context.hash = hash;
+	return context;
 };
