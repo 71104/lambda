a88039425170577e3bbbbe8f8f970fb4d319dea7
Alberto La Rocca <a71104@gmail.com>
2015-05-30 10:43:03 +0200

    Bugfix

--- src/Utilities.js
+++ src/Utilities.js
@@ -1,10 +1,15 @@
 Array.prototype.union = function (other) {
+	var prefix = 'SET ';
 	var hash = {};
 	for (var i = 0; i < this.length; i++) {
-		hash[this[i]] = true;
+		hash[prefix + this[i]] = true;
 	}
 	for (var j = 0; j < other.length; j++) {
-		hash[other[j]] = true;
+		hash[prefix + other[j]] = true;
 	}
-	return Object.keys(hash);
+	return Object.keys(hash).filter(function (key) {
+		return key.substr(0, prefix.length) === prefix;
+	}).map(function (key) {
+		return key.substr(prefix.length);
+	});
 };
