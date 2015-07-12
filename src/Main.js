34a602de36183dff5da016fcd0d6e01a6b14c42c
Alberto La Rocca <a71104@gmail.com>
2015-07-12 19:50:31 +0200

    New command line script

--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,35 @@
 #!/usr/bin/env node
-var Lambda = require('./lambda.js');
 
-require('repl').start({
-	eval: function (input, context, fileName, callback) {
-		try {
-			var ast = (new Lambda.Parser(input)).parse();
-			var value = ast.evaluate(new Lambda.DefaultContext());
-			callback(value.toString());
-		} catch (e) {
-			callback(e);
+var lambda = (function () {
+	var Lambda = require('./lambda.js');
+	var context = new Lambda.DefaultContext();
+	return function (input) {
+		return (new Lambda.Parser(input)).parse().evaluate(context);
+	};
+}());
+
+if (process.stdin.isTTY) {
+	require('repl').start({
+		eval: function (input, context, fileName, callback) {
+			try {
+				callback(lambda(input).toString());
+			} catch (e) {
+				callback(e);
+			}
 		}
-	}
-});
+	});
+} else {
+	process.stdin.setEncoding('ascii');
+
+	var input = '';
+
+	process.stdin.on('data', function (text) {
+		input += text;
+	}).on('end', function () {
+		try {
+			process.stdout.write(lambda(input).toString());
+		} catch (e) {
+			process.stderr.write(e);
+		}
+	});
+}
