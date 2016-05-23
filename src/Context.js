23bed9c2492bae6ed9ccd8d266c4d54de612adc5
Alberto La Rocca <a71104@gmail.com>
2016-05-23 14:16:24 +0100

    Fixes #75 once and for all

--- src/Context.js
+++ src/Context.js
@@ -4,12 +4,20 @@ function Context(hash) {
 
 exports.Context = Context;
 
+Context.prototype._marshal = function (value) {
+  return value;
+};
+
+Context.prototype._unmarshal = function (value) {
+  return value;
+};
+
 Context.prototype.has = function (name) {
   return name in this._hash;
 };
 
 Context.prototype.top = function (name) {
-  return this._hash[name];
+  return this._unmarshal(this._hash[name]);
 };
 
 Context.prototype.names = function () {
@@ -24,13 +32,13 @@ Context.prototype.names = function () {
 Context.prototype.forEach = function (callback, context) {
   for (var name in this._hash) {
     // jshint forin: false
-    callback.call(context, name, this._hash[name]);
+    callback.call(context, name, this._unmarshal(this._hash[name]));
   }
 };
 
 Context.prototype.add = function (name, value) {
   var hash = Object.create(this._hash);
-  hash[name] = value;
+  hash[name] = this._marshal(value);
   return new Context(hash);
 };
 
@@ -38,7 +46,7 @@ Context.prototype.addAll = function (hash) {
   var child = Object.create(this._hash);
   for (var name in hash) {
     if (hash.hasOwnProperty(name)) {
-      child[name] = hash[name];
+      child[name] = this._marshal(hash[name]);
     }
   }
   return new Context(child);
@@ -47,7 +55,7 @@ Context.prototype.addAll = function (hash) {
 Context.prototype.extend = function (context) {
   var child = Object.create(this._hash);
   context.forEach(function (name, value) {
-    child[name] = value;
+    child[name] = this._marshal(value);
   });
   return new Context(child);
 };
@@ -56,55 +64,18 @@ Context.EMPTY = new Context();
 
 
 function NativeContext(object) {
+  Context.call(this, object);
   this.object = object;
 }
 
 exports.NativeContext = NativeContext;
 
-NativeContext.prototype.has = function (name) {
-  return name in this.object;
+NativeContext.prototype = Object.create(Context.prototype);
+
+NativeContext.prototype._marshal = function (value) {
+  return value.marshal();
 };
 
-NativeContext.prototype.top = function (name) {
-  return AbstractValue.unmarshal(this.object[name]);
-};
-
-NativeContext.prototype.names = function () {
-  var names = [];
-  for (var name in this.object) {
-    // jshint forin: false
-    names.push(name);
-  }
-  return names;
-};
-
-NativeContext.prototype.forEach = function (callback, context) {
-  for (var name in this.object) {
-    // jshint forin: false
-    callback.call(context, name, AbstractValue.unmarshal(this.object[name]));
-  }
-};
-
-NativeContext.prototype.add = function (name, value) {
-  var object = Object.create(this.object);
-  object[name] = value.marshal();
-  return new NativeContext(object);
-};
-
-NativeContext.prototype.addAll = function (hash) {
-  var child = Object.create(this.object);
-  for (var name in hash) {
-    if (hash.hasOwnProperty(name)) {
-      child[name] = hash[name].marshal();
-    }
-  }
-  return new NativeContext(child);
-};
-
-NativeContext.prototype.extend = function (context) {
-  var child = Object.create(this.object);
-  context.forEach(function (name, value) {
-    child[name] = value;
-  });
-  return new NativeContext(child);
+NativeContext.prototype._unmarshal = function (value) {
+  return AbstractValue.unmarshal(value);
 };
