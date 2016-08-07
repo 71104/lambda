5753432ed5a728870df940430ab8232896f2039c
Alberto La Rocca <a71104@gmail.com>
2016-08-07 22:51:35 +0100

    Values

---
+++ src/Context.js
@@ -0,0 +1,55 @@
+function Context(hash) {
+  this._hash = hash || Object.create(null);
+}
+
+exports.Context = Context;
+
+Context.prototype._marshal = function (value) {
+  return value;
+};
+
+Context.prototype._unmarshal = function (value) {
+  return value;
+};
+
+Context.prototype.has = function (name) {
+  return name in this._hash;
+};
+
+Context.prototype.top = function (name) {
+  return this._unmarshal(this._hash[name]);
+};
+
+Context.prototype.add = function (name, value) {
+  var hash = Object.create(this._hash);
+  hash[name] = this._marshal(value);
+  return new Context(value);
+};
+
+Context.prototype.addAll = function (map) {
+  var hash = Object.create(this._hash);
+  for (var key in map) {
+    if (map.hasOwnProperty(key)) {
+      hash[key] = this._marshal(map[key]);
+    }
+  }
+  return new Context(hash);
+};
+
+Context.EMPTY = new Context;
+
+
+function NativeContext(object) {
+  Context.call(this, object);
+}
+
+exports.NativeContext = NativeContext;
+extend(Context, NativeContext);
+
+NativeContext.prototype._marshal = function (value) {
+  return value.marshal();
+};
+
+NativeContext.prototype._unmarshal = function (value) {
+  return UndefinedValue.unmarshal(value);
+};
---
+++ src/Values.js
@@ -0,0 +1,97 @@
+function UndefinedValue() {}
+
+exports.UndefinedValue = UndefinedValue;
+
+UndefinedValue.prototype.context = Context.EMPTY;
+
+
+function ComplexValue(real, imaginary) {
+  real = +real;
+  imaginary = +imaginary;
+  UndefinedValue.call(this);
+  this.real = real;
+  this.imaginary = imaginary;
+}
+
+exports.ComplexValue = ComplexValue;
+extend(UndefinedValue, ComplexValue);
+
+
+function RealValue(value) {
+  value = +value;
+  ComplexValue.call(this, value, 0);
+  this.value = value;
+}
+
+exports.RealValue = RealValue;
+extend(ComplexValue, RealValue);
+
+
+function IntegerValue(value) {
+  RealValue.call(this, ~~value);
+}
+
+exports.IntegerValue = IntegerValue;
+extend(RealValue, IntegerValue);
+
+
+function NaturalValue(value) {
+  value = ~~value;
+  if (value < 0) {
+    throw new LambdaInternalError();
+  }
+  IntegerValue.call(this, value);
+}
+
+exports.NaturalValue = NaturalValue;
+extend(IntegerValue, NaturalValue);
+
+
+function BooleanValue(value) {
+  value = !!value;
+  UndefinedValue.call(this);
+}
+
+exports.BooleanValue = BooleanValue;
+expand(UndefinedValue, BooleanValue);
+
+
+function IndexedValue() {
+  UndefinedValue.call(this);
+}
+
+exports.IndexedValue = IndexedValue;
+extend(UndefinedValue, IndexedValue);
+
+
+function StringValue(value) {
+  value = '' + value;
+  IndexedValue.call(this);
+  this.value = value;
+}
+
+exports.StringValue = StringValue;
+extend(IndexedValue, StringValue);
+
+StringValue.prototype.lookup = function (index) {
+  if (index < 0 || index >= this.value.length) {
+    throw new LambdaRuntimeError();
+  }
+  return new StringValue(this.value[index]);
+};
+
+
+function ListValue(values) {
+  IndexedValue.call(this);
+  this.values = values;
+}
+
+exports.ListValue = ListValue;
+extend(IndexedValue, ListValue);
+
+ListValue.prototype.lookup = function (index) {
+  if (index < 0 || index >= this.values.length) {
+    throw new LambdaRuntimeError();
+  }
+  return this.values[index];
+};
