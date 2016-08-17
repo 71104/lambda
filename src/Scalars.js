1ae345438e0fd68f7fa0f51de31e515c8a940b4c
Alberto La Rocca <a71104@gmail.com>
2016-08-17 10:52:16 +0100

    Math functions

--- src/Scalars.js
+++ src/Scalars.js
@@ -17,6 +17,55 @@ ComplexValue.prototype.context = ComplexValue.prototype.context.addAll({
 });
 
 
+RealType.prototype.context = RealType.prototype.context.addAll({
+  abs: RealType.DEFAULT,
+  ceil: IntegerType.DEFAULT,
+  floor: IntegerType.DEFAULT,
+  round: IntegerType.DEFAULT,
+  trunc: IntegerType.DEFAULT,
+  sqrt: RealType.DEFAULT,
+  cbrt: RealType.DEFAULT,
+  exp: RealType.DEFAULT,
+  expm1: RealType.DEFAULT,
+  log: RealType.DEFAULT,
+  log10: RealType.DEFAULT,
+  log2: RealType.DEFAULT,
+  sin: RealType.DEFAULT,
+  cos: RealType.DEFAULT,
+  tan: RealType.DEFAULT,
+  asin: RealType.DEFAULT,
+  acos: RealType.DEFAULT,
+  atan: RealType.DEFAULT,
+});
+
+function _makeMathFunction(name) {
+  return Closure.fromFunction(function () {
+    return Math[name](this);
+  });
+}
+
+RealValue.prototype.context = RealValue.prototype.context.addAll({
+  abs: _makeMathFunction('abs'),
+  ceil: _makeMathFunction('ceil'),
+  floor: _makeMathFunction('floor'),
+  round: _makeMathFunction('round'),
+  trunc: _makeMathFunction('trunc'),
+  sqrt: _makeMathFunction('sqrt'),
+  cbrt: _makeMathFunction('cbrt'),
+  exp: _makeMathFunction('exp'),
+  expm1: _makeMathFunction('expm1'),
+  log: _makeMathFunction('log'),
+  log10: _makeMathFunction('log10'),
+  log2: _makeMathFunction('log2'),
+  sin: _makeMathFunction('sin'),
+  cos: _makeMathFunction('cos'),
+  tan: _makeMathFunction('tan'),
+  asin: _makeMathFunction('asin'),
+  acos: _makeMathFunction('acos'),
+  atan: _makeMathFunction('atan'),
+});
+
+
 BooleanType.prototype.context = BooleanType.prototype.context.addAll({
   str: StringType.DEFAULT,
 });
