c29c731e581f7f2c7f5aadd4d9df6e9c8d7d75ea
Alberto La Rocca <a71104@gmail.com>
2016-09-03 15:09:10 +0100

    Boolean NOT

--- src/DefaultContext.js
+++ src/DefaultContext.js
@@ -2,10 +2,18 @@ exports.DefaultContext = DefaultContext = {};
 
 DefaultContext.TYPES = Context.EMPTY.addAll({
   'typeof': new LambdaType(new VariableType('T'), StringType.DEFAULT),
+  'not': new LambdaType(BooleanType.DEFAULT, BooleanType.DEFAULT),
 });
 
 DefaultContext.VALUES = Context.EMPTY.addAll({
   'typeof': Closure.fromFunction(function (value) {
     return new StringValue(characterToString(value.character));
+  }),
+  'not': Closure.fromFunction(function (x) {
+    if (x.is(BooleanValue)) {
+      return new BooleanValue(!x.value);
+    } else {
+      throw new LambdaRuntimeError();
+    }
   }),
 });
