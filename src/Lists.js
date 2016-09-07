3a1961a9452175c8f9a3dd8b80224cce3022e6d6
Alberto La Rocca <a71104@gmail.com>
2016-09-07 20:01:04 +0100

    Lists

--- src/Lists.js
+++ src/Lists.js
@@ -1,6 +1,17 @@
 ListType.prototype.context = ListType.prototype.context.addAll({
   length: NaturalType.DEFAULT,
 
+  head: new LambdaType(
+      new ListType(
+          new VariableType('T')),
+      new VariableType('T')),
+
+  tail: new LambdaType(
+      new ListType(
+          new VariableType('T')),
+      new ListType(
+          new VariableType('T'))),
+
   reverse: new LambdaType(
     new ListType(
       new VariableType('T')),
@@ -17,6 +28,30 @@ ListType.prototype.context = ListType.prototype.context.addAll({
           new VariableType('T'), BooleanType.DEFAULT)),
       new ListType(
         new VariableType('T')))),
+
+  each: new LambdaType(
+      new ListType(
+          new VariableType('T')),
+      new LambdaType(
+          new LambdaType(
+              new VariableType('T'), UndefinedType.DEFAULT),
+          UndefinedType.DEFAULT)),
+
+  some: new LambdaType(
+      new ListType(
+          new VariableType('T')),
+      new LambdaType(
+          new LambdaType(
+              new VariableType('T'), BooleanType.DEFAULT),
+          BooleanType.DEFAULT)),
+
+  every: new LambdaType(
+      new ListType(
+          new VariableType('T')),
+      new LambdaType(
+          new LambdaType(
+              new VariableType('T'), BooleanType.DEFAULT),
+          BooleanType.DEFAULT)),
 
   map: new LambdaType(
     new ListType(
@@ -44,10 +79,27 @@ ListValue.prototype.context = ListValue.prototype.context.addAll({
   length: Closure.fromFunction(function (list) {
     return new NaturalValue(list.values.length);
   }),
+  head: Closure.fromFunction(function (list) {
+    if (list.values.length) {
+      return list.values[0];
+    } else {
+      throw new LambdaRuntimeError();
+    }
+  }),
+  tail: Closure.fromFunction(function (list) {
+    if (list.values.length) {
+      return new ListValue(list.values.slice(1));
+    } else {
+      throw new LambdaRuntimeError();
+    }
+  }),
   reverse: Closure.fromFunction(function (list) {
     return new ListValue(list.values.reverse());
   }),
   sort: Closure.fromFunction(function (list, lambda) {
+    if (!lambda.is(Closure)) {
+      throw new LambdaRuntimeError();
+    }
     var compare = function (a, b) {
       var result = lambda.apply(a, b);
       if (result.is(BooleanValue)) {
@@ -66,19 +118,65 @@ ListValue.prototype.context = ListValue.prototype.context.addAll({
       }
     }));
   }),
+  each: Closure.fromFunction(function (list, callback) {
+    if (callback.is(Closure)) {
+      list.values.forEach(function (element) {
+        callback.apply(element);
+      });
+      return UndefinedValue.DEFAULT;
+    } else {
+      throw new LambdaRuntimeError();
+    }
+  }),
+  some: Closure.fromFunction(function (list, callback) {
+    if (callback.is(Closure)) {
+      return new BooleanValue(list.values.some(function (element) {
+        var result = callback.apply(element);
+        if (result.is(BooleanValue)) {
+          return result.value;
+        } else {
+          throw new LambdaRuntimeError();
+        }
+      }));
+    } else {
+      throw new LambdaRuntimeError();
+    }
+  }),
+  every: Closure.fromFunction(function (list, callback) {
+    if (callback.is(Closure)) {
+      return new BooleanValue(list.values.every(function (element) {
+        var result = callback.apply(element);
+        if (result.is(BooleanValue)) {
+          return result.value;
+        } else {
+          throw new LambdaRuntimeError();
+        }
+      }));
+    } else {
+      throw new LambdaRuntimeError();
+    }
+  }),
   map: Closure.fromFunction(function (list, callback) {
-    return new ListValue(list.values.map(function (element) {
-      return callback.apply(element);
-    }));
+    if (callback.is(Closure)) {
+      return new ListValue(list.values.map(function (element) {
+        return callback.apply(element);
+      }));
+    } else {
+      throw new LambdaRuntimeError();
+    }
   }),
   filter: Closure.fromFunction(function (list, callback) {
-    return new ListValue(list.values.filter(function (element) {
-      var result = callback.apply(element);
-      if (result.is(BooleanValue)) {
-        return result.value;
-      } else {
-        throw new LambdaRuntimeError();
-      }
-    }));
+    if (callback.is(Closure)) {
+      return new ListValue(list.values.filter(function (element) {
+        var result = callback.apply(element);
+        if (result.is(BooleanValue)) {
+          return result.value;
+        } else {
+          throw new LambdaRuntimeError();
+        }
+      }));
+    } else {
+      throw new LambdaRuntimeError();
+    }
   }),
 });
